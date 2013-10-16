9a875fdfe663178b74feaf9bb3190226b1e37545
Adam Keys <adam@therealadam.com>
2013-10-15 20:20:05 -0500

    Initial Commit

---
+++ kvc-demo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  kvc-demo
+//
+//  Created by Adam Keys on 10/15/13.
+//  Copyright (c) 2013 Adam Keys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
